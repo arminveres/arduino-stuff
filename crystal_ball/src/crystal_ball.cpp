6c21c651d7b16ece936f8406856e4a3470c61822
arminveres <armin.veres@hotmail.com>
2022-08-19 13:43:52 +0200

    Initial commit for LCD Cpp branch

---
+++ crystal_ball/src/crystal_ball.cpp
@@ -0,0 +1,78 @@
+#include "LiquidCrystal.h"
+#include "arduino.h"
+#include <util/delay.h>
+
+using namespace Arduino::Uno;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+constexpr int switchPin = 6;
+int switchState = 0;
+int prevSwitchState = 0;
+int reply = 0;
+
+String answers[] = {
+    "hello there",
+    "General Kenobi",
+    "This is where the fun begins",
+    "Do or do not. There is no try.",
+};
+
+// risky to do, better define an predefined array length
+constexpr int LENGTH_ANSWERS = sizeof(answers) / sizeof(answers[0]);
+
+/*
+ * @brief print answer to lcd screen and split if necessary
+ */
+void printAnswer(String string) {
+  if (string.length() >= 16 && string.length() <= 32) {
+    String partOne = string.substring(0, 16);
+    String partTwo = string.substring(16, 31);
+    lcd.setCursor(0, 0);
+    lcd.print(partOne);
+    lcd.setCursor(0, 1);
+    lcd.print(partTwo);
+
+  } else if (string.length() < 16) {
+    lcd.print(string);
+  } else if (string.length() > 32) {
+    lcd.print("the wisdom of");
+    lcd.print("the ball too great");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  pinMode(switchPin, INPUT);
+  lcd.print("Ask the Crystal ");
+  lcd.setCursor(0, 1);
+  lcd.print("Ball");
+
+  while (true) {
+
+    // put your main code here, to run repeatedly:
+    switchState = digitalRead(switchPin);
+    if (switchState != prevSwitchState) {
+      if (switchState == LOW) {
+        // reply = random(LENGTH_ANSWERS);
+        const int reply = 1;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("The ball says");
+        _delay_ms(500);
+        lcd.print(".");
+        _delay_ms(500);
+        lcd.print(".");
+        _delay_ms(500);
+        lcd.print(".");
+        _delay_ms(500);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        printAnswer(answers[reply]);
+      }
+    }
+    prevSwitchState = switchState;
+  }
+  return 0;
+}
---
+++ include/arduino.h
@@ -0,0 +1,18 @@
+/*
+ * These are definitions akin to the Arduino Library, for ease of use
+ */
+#include <stdint.h>
+
+namespace Arduino {
+namespace Uno {
+
+constexpr uint8_t INPUT = 0;
+constexpr uint8_t OUTPUT = 1;
+constexpr uint8_t INPUT_PULLUP = 2;
+
+// TODO:digital read
+// TODO:digital write
+// TODO:set pin mode
+
+} // namespace Uno
+} // namespace Arduino
